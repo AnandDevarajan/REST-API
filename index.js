7e14f263aa518b4f412aecb4abc4bb190df63adb
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-14 21:55:08 +0530

    product model added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('expres');
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 const productRoutes = require('./routes/product');
 const orderRoutes = require('./routes/order');
+const mongoose = require('mongoose');
 const app = express();
 
 const port = process.env.port || 3000;
+
+mongoose
+  .connect(process.env.MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log('DB CONNECTED'))
+  .catch((err) => console.log(err.message));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
