3c87e5c8758d435b8752486c465db8939db1867a
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-16 22:56:50 +0530

    order route

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,41 @@ const { check, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const Order = require('../models/Order');
 
+//@router   GET
+//@desc     to get all orders
+//@access   PRIVATE
+router.get('/order', (req, res) => {
+  Order.find()
+    .select('-__v')
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          message: 'No Order found',
+        });
+      }
+      return res.json({
+        message: 'Your Orders',
+        orders,
+      });
+    });
+});
+
+//@router   GET
+//@desc     to get an individual order
+//@access   PRIVATE
+router.get('/order/:id', (req, res) => {
+  Order.findById(req.params.id)
+    .select('-__v')
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          message: 'Order not found',
+        });
+      }
+      res.json(order);
+    });
+});
+
 //@router   POST
 //@desc     to add an order
 //@access   PRIVATE
