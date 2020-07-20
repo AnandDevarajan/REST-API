4143a4ce63b0a98b975ced6d1e1482a11f3cd2ce
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-20 20:50:36 +0530

    added options to multer

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Product = require('../models/Product');
 const { check, validationResult } = require('express-validator');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-const mongoose = require('mongoose');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    return cb(null, true);
+  }
+  cb(null, false);
+};
+const upload = multer({
+  dest: 'uploads/',
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: 1024 * 1024 * 3,
+});
 
 //@router   GET
 //@desc     to get all products
